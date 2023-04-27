babeb27f24788195dc197e99261835dae5b65c8a
Guilherme Carvalho <guilherme.carvalhode@gmail.com>
2023-04-27 11:13:58 -0300

    feat: add ExpressRouteAdapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,13 @@
+import { type Request, type Response } from 'express'
+import { type Controller, type HttpRequest } from '../../presentation/protocols'
+
+export const adaptRoute = (controller: Controller) => {
+  return async (req: Request, res: Response): Promise<void> => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+
+    const httpResponse = await controller.handle(httpRequest)
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
