bd602eba166d945ab99925c97b03c87ad32cacbd
Guilherme Carvalho <guilherme.carvalhode@gmail.com>
2023-06-03 14:33:19 -0300

    feat: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'e382833135a6606c7c6b8b7697a239c5'
 }
