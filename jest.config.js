ca87cf681c61e104395a842fbe422cade0eacadf
Guilherme Carvalho <guilherme.carvalhode@gmail.com>
2023-06-03 15:09:42 -0300

    chore: add paths to jest ignore on coverage

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,11 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/**-protocols.ts',
+    '!<rootDir>/src/presentation/protocols/**',
+    '!<rootDir>/src/domain/**',
+    '!<rootDir>/src/data/protocols/**'
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
