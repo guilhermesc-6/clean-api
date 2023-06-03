67aa482d176e0d3c6541308091a096e4f415cbca
Guilherme Carvalho <guilherme.carvalhode@gmail.com>
2023-06-03 11:58:11 -0300

    test: ensure AccountMongoRepository updates the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -61,4 +61,24 @@ describe('Mongo Repository', () => {
 
     expect(account).toBeFalsy()
   })
+
+  it('Should update the account accessToken on updateAccessToken success', async () => {
+    const sut = makeSut()
+
+    const res = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+
+    const rawAccount = await accountCollection.findOne({ _id: res.insertedId })
+    expect(rawAccount.accessToken).toBeFalsy()
+
+    await sut.updateAccessToken(res.insertedId.toString(), 'any_token')
+
+    const account = await accountCollection.findOne({ _id: res.insertedId })
+
+    expect(account).toBeTruthy()
+    expect(account.accessToken).toBeTruthy()
+  })
 })
