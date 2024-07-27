1cc8ba4b7f0cd08bf311203f9581a0f0851b7bff
Guilherme Carvalho <guilherme.carvalhode@gmail.com>
2024-07-27 13:07:32 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -21,8 +21,12 @@ export const MongoHelper = {
     return this.client.db().collection(name)
   },
 
-  map (collection: any): any {
-    const { _id, ...account } = collection
-    return { id: _id, ...account }
+  map (data: any): any {
+    const { _id, ...rest } = data
+    return { ...rest, id: _id.toHexString() }
+  },
+
+  mapCollection: (collection: any[]): any[] => {
+    return collection.map(c => MongoHelper.map(c))
   }
 }
