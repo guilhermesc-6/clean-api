d41fbe4678601cce984ef31b86d7598549f9d104
Guilherme Carvalho <guilherme.carvalhode@gmail.com>
2024-08-03 11:25:10 -0300

    feat: ensure save survey result route returns 403 if no token is provided

--- src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-controller-factory.ts
@@ -1,6 +1,6 @@
 import { makeAddSurveyValidation } from './add-survey-validation-factory'
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
-import { makeDbAddSurvey } from '@/main/factories/usecases/survey/add-survey/db-add-survey'
+import { makeDbAddSurvey } from '@/main/factories/usecases/survey/add-survey/db-add-survey-factory'
 import { AddSurveyController } from '@/presentation/controllers/survey/add-survey/add-survey-controller'
 import type { Controller } from '@/presentation/protocols'
 
