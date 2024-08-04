5c123bafb4384a4ed8a994c97e21e0aa9b71c1e8
Guilherme Carvalho <guilherme.carvalhode@gmail.com>
2024-08-04 12:13:07 -0300

    feat: add swagger api

---
+++ src/main/config/config-swagger.ts
@@ -0,0 +1,7 @@
+import sweggerConfig from '@/main/docs'
+import { serve, setup } from 'swagger-ui-express'
+import type { Express } from 'express'
+
+export default (app: Express): void => {
+  app.use('/api-docs', serve, setup(sweggerConfig))
+}
