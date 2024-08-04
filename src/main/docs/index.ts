d0de98fc7f0d5223d52ac64dab18208e70deabd9
Guilherme Carvalho <guilherme.carvalhode@gmail.com>
2024-08-04 14:37:55 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,27 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamsSchema } from './schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     version: '1.0.0',
     title: 'Clean Node API',
     description: 'API do curso do Mango para realizar enquetes entre programadores'
+  },
+  servers: [
+    {
+      url: '/api'
+    }
+  ],
+  tags: [
+    { name: 'Login' }
+  ],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
