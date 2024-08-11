db1e7612a8e054c6790a451053989866a77e2018
Guilherme Carvalho <guilherme.carvalhode@gmail.com>
2024-08-11 16:00:46 -0300

    test: ensure load survey route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -81,5 +81,26 @@ describe('Survey Results Routes', () => {
         .get('/api/surveys/any_id/results')
         .expect(403)
     })
+
+    it('Should return 200 on load survey results with accessToken', async () => {
+      const accessToken = await makeAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [
+          {
+            image: 'http://image-name.com',
+            answer: 'Answer 1'
+          },
+          {
+            answer: 'Answer 2'
+          }],
+        date: new Date()
+      })
+
+      await request(app)
+        .get(`/api/surveys/${res.insertedId.toHexString()}/results`)
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
   })
 })
