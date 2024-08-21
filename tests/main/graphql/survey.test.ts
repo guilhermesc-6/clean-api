61f0f66ef897295aa928e4afeaaa372049731d1c
Guilherme Carvalho <guilherme.carvalhode@gmail.com>
2024-08-21 13:43:41 -0300

    test: ensure surveys query return AccessDeniedError if no token is provided

--- tests/main/graphql/survey.test.ts
+++ tests/main/graphql/survey.test.ts
@@ -94,5 +94,26 @@ describe('Survey GraphQL', () => {
         image: null
       }])
     })
+
+    it('Should return AccessDeniedError if no token is provided', async () => {
+      const now = new Date()
+      await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: now
+      })
+
+      const res = await request(app)
+        .post('/graphql')
+        .send({ query })
+      expect(res.status).toBe(403)
+      expect(res.body.data).toBeFalsy()
+      expect(res.body.errors[0].message).toBe('Access denied')
+    })
   })
 })
