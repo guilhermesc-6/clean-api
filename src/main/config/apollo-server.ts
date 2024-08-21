17bc9381067466400ad8465cf008ffd0e5b7e683
Guilherme Carvalho <guilherme.carvalhode@gmail.com>
2024-08-21 12:21:12 -0300

    feat: add auth directive to survey route

--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -1,13 +1,19 @@
 import resolvers from '../graphql/resolvers'
 import typeDefs from '../graphql/type-defs'
 import { statusCodePlugin } from '../graphql/plugins'
+import { authDirectiveTransformer } from '@/main/graphql/directives'
+
+import { makeExecutableSchema } from '@graphql-tools/schema'
 import { ApolloServer } from 'apollo-server-express'
 import type { Express } from 'express'
 
 export default (app: Express): void => {
+  let schema = makeExecutableSchema({ resolvers, typeDefs })
+  schema = authDirectiveTransformer(schema)
+
   const server = new ApolloServer({
-    resolvers,
-    typeDefs,
+    schema,
+    context: ({ req }) => ({ req }),
     plugins: [statusCodePlugin]
   })
   server.start().then(() => {
